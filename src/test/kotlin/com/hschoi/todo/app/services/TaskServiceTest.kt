30f23da692519b0322a1e4f9c8ec4256b464e1a4
nate <nate@dunamu.com>
2020-04-11 22:15:34 +0900

    전체조회, 단건 할일 등록 성공 추가.

--- src/test/kotlin/com/hschoi/todo/app/services/TaskServiceTest.kt
+++ src/test/kotlin/com/hschoi/todo/app/services/TaskServiceTest.kt
@@ -1,19 +1,28 @@
 package com.hschoi.todo.app.services
 
+import com.hschoi.todo.app.dto.request.TaskRequestDto
 import com.hschoi.todo.app.entities.SubTask
 import com.hschoi.todo.app.entities.Task
 import com.hschoi.todo.app.repository.SubTaskRepository
 import com.hschoi.todo.app.repository.TaskRepository
 import com.hschoi.todo.common.code.TaskStatus
+import com.hschoi.todo.common.exception.BizException
+import com.hschoi.todo.common.utils.PageRequest
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.assertj.core.api.BDDAssertions.then
 import org.mockito.BDDMockito.given
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import org.modelmapper.ModelMapper
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import java.util.*
+
 
 @ExtendWith(MockitoExtension::class)
 internal class TaskServiceTest {
@@ -88,4 +97,69 @@ internal class TaskServiceTest {
 
     }
 
+    @Test
+    fun `단일 Task_단건_조회_실패`() {
+        Assertions.assertThrows(BizException::class.java, { taskService.findById(1L) })
+    }
+
+    @Test
+    fun `단일 Task_전체_조회_성공`() {
+        // init
+        val id = 1L
+        var size = 10
+        var page = 1
+
+        val createdTestTask1 = Task(
+            id = 1L, title = "할일 조회",
+            description = "할일 조회 테스트를 만든다", taskStatus = TaskStatus.TODO
+        )
+
+        val createdTestTask2 = Task(
+            id = 2L, title = " 할일 전체 조회",
+            description = "할일 전체 조회 테스트를 만든다", taskStatus = TaskStatus.TODO
+        )
+        val result: List<Task> = mutableListOf(createdTestTask1,createdTestTask2)
+        val pageable: Pageable = PageRequest.of(page, size)
+        var taskList: Page<Task> = PageImpl(result, pageable, 2L);
+
+        //given
+        given(taskRepository.findAll(PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id")))).willReturn(taskList)
+
+        //when
+        val foundTask = taskService.findAll(page, size)
+
+        //then
+        then(foundTask.totalElements).isEqualTo(taskList.totalElements)
+        then(foundTask.content.get(0).title).isEqualTo("할일 조회")
+        then(foundTask.content.get(1).title).contains("할일 전체 조회")
+    }
+
+    @Test
+    fun `단일 Task_등록_성공`() {
+        val createdTestTask = Task(
+            id = 1L, title = "할일 등록",
+            description = "할일 등록 테스트를 만든다", taskStatus = TaskStatus.TODO
+        )
+
+        //given
+        given(
+            taskRepository.save(
+                Task(
+                    id = 1L, title = "할일 등록",
+                    description = "할일 등록 테스트를 만든다", taskStatus = TaskStatus.TODO
+                )
+            )
+        ).willReturn(createdTestTask)
+
+        //when
+        val createdTask = taskService.registerTask(TaskRequestDto(subTaskNo = null, title = "할일 등록",
+            description = "할일 등록 테스트를 만든다",taskStatus = TaskStatus.TODO,complatedAt = null))
+
+        //then
+        then(createdTask.id).isEqualTo(createdTestTask.id)
+        then(createdTask.title).isEqualTo(createdTestTask.title)
+        then(createdTask.description).isEqualTo(createdTestTask.description)
+        then(createdTask.complatedAt).isNull()
+
+    }
 }
